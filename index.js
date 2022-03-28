b2e0f65eea886bb05a40bed600bbb515c331241d
Lash Blast <evelyn.aguirre.9895@gmail.com>
2022-03-28 12:51:23 -0500

    write initial questions

--- index.js
+++ index.js
@@ -5,6 +5,59 @@ const inquirer = require('inquirer');
 const questions = [];
 
 
+//  , Installation, Usage, License, Contributing, Tests, and Questions
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      // title of project
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project? (Required)',
+      validate: titleInput => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log('Please enter your project title!');
+          return false;
+        }
+      }
+    },
+    {
+      // description of project
+      type: 'input',
+      name: 'description',
+      message: 'Enter a description of your project (Required)',
+      validate: projectDescriptionInput => {
+        if (projectDescriptionInput) {
+          return true;
+        } else {
+          console.log('Please enter your project description!');
+          return false;
+        }
+      }
+    },
+    {
+      // table of contents prompt
+      type: 'confirm',
+      name: 'confirmTOC',
+      message: 'Would you like to add a Table of Contents?',
+      default: true
+    },
+    {
+      type: 'input',
+      name: 'tableOfContents',
+      message: 'Provide your Table of Contents:',
+      when: ({ confirmTableOfContents }) => {
+        if (confirmTableOfContents) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }
+  ]);
+};
+
 
 
 // TODO: Create a function to write README file
