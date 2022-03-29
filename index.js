7bdaa0bdcfe73befbdd3546ab2a2186178510d8c
Lash Blast <evelyn.aguirre.9895@gmail.com>
2022-03-29 10:35:56 -0500

    work on markdown

--- index.js
+++ index.js
@@ -2,173 +2,174 @@
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+    // title of project
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project? (Required)',
+    validate: titleInput => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log('Please enter your project title!');
+        return false;
+      }
+    }
+  },
+  {
+    // description of project
+    type: 'input',
+    name: 'description',
+    message: 'Enter a description of your project (Required)',
+    validate: projectDescriptionInput => {
+      if (projectDescriptionInput) {
+        return true;
+      } else {
+        console.log('Please enter your project description!');
+        return false;
+      }
+    }
+  },
+  {
+    // table of contents prompt
+    type: 'confirm',
+    name: 'confirmTOC',
+    message: 'Would you like to add a Table of Contents?',
+    default: true
+  },
+  {
+    // Installation section prompt
+    type: 'confirm',
+    name: 'confirmInstallation',
+    message: 'Would you like to add an Installation section?',
+    default: true
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide your Installation instructions:',
+    when: ({ confirmInstallation }) => {
+      if (confirmInstallation) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  },
+  {
+    // Usage section prompt
+    type: 'confirm',
+    name: 'confirmUsage',
+    message: 'Would you like to include Usage instructions?',
+    default: true
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide your Usage instructions:',
+    when: ({ confirmUsage }) => {
+      if (confirmUsage) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  },
+  {
+    // license information
+    type: 'checkbox',
+    name: 'licenses',
+    message: 'Which licenses did you need for this project? (Check all that apply)',
+    choices: ['MIT', 'ISC', 'Apache', 'Cloud Native Computing', 'GNU']
 
+    // Apache requires Apache License 2.0
+    // Cloud Native Computing Foundation dictates Apache License 2.0 by default
+    // GNU recommends GNU GPLv3 for most programs
+    // npm packages overwhelmingly use the MIT or the very similar ISC licenses
+    // OpenBSD prefers the ISC License
+    // Rust crates are overwhelmingly licensed under both MIT and Apache License 2.0
+    // WordPress plugins and themes must be GNU GPLv2 (or later)
 
-const promptUser = () => {
+  },
+  {
+    // Contributing section prompt
+    type: 'confirm',
+    name: 'confirmContribute',
+    message: 'Would you like to include a Contribute section?',
+    default: true
+  },
+  {
+    type: 'input',
+    name: 'contribute',
+    message: 'Provide your Contribute information:',
+    when: ({ confirmContribute }) => {
+      if (confirmContribute) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  },
+  // Questions section
+  {
+    // add GitHub user name
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub Username (Required)',
+    validate: userNameInput => {
+      if (userNameInput) {
+        return true;
+      } else {
+        console.log('Please enter your user name!');
+        return false;
+      }
+    }
+  },
+  {
+    // add GitHub link
+    type: 'input',
+    name: 'link',
+    message: 'Enter the GitHub link to your project. (Required)',
+    validate: linkInput => {
+      if (linkInput) {
+        return true;
+      } else {
+        console.log('Please enter your GitHub link!');
+        return false;
+      }
+    }
+  },
+  {
+    // add email
+    type: 'input',
+    name: 'email',
+    message: 'Enter you email address. (Required)',
+    validate: emailInput => {
+      if (emailInput) {
+        return true;
+      } else {
+        console.log('Please enter your email address!');
+        return false;
+      }
+    }
+  },
+];
 
-  return inquirer.prompt([
-    {
-      // title of project
-      type: 'input',
-      name: 'title',
-      message: 'What is the title of your project? (Required)',
-      validate: titleInput => {
-        if (titleInput) {
-          return true;
-        } else {
-          console.log('Please enter your project title!');
-          return false;
-        }
-      }
-    },
-    {
-      // description of project
-      type: 'input',
-      name: 'description',
-      message: 'Enter a description of your project (Required)',
-      validate: projectDescriptionInput => {
-        if (projectDescriptionInput) {
-          return true;
-        } else {
-          console.log('Please enter your project description!');
-          return false;
-        }
-      }
-    },
-    {
-      // table of contents prompt
-      type: 'confirm',
-      name: 'confirmTOC',
-      message: 'Would you like to add a Table of Contents?',
-      default: true
-    },
-    {
-      // Installation section prompt
-      type: 'confirm',
-      name: 'confirmInstallation',
-      message: 'Would you like to add an Installation section?',
-      default: true
-    },
-    {
-      type: 'input',
-      name: 'installation',
-      message: 'Provide your Installation instructions:',
-      when: ({ confirmInstallation }) => {
-        if (confirmInstallation) {
-          return true;
-        } else {
-          return false;
-        }
-      }
-    },
-    {
-      // Usage section prompt
-      type: 'confirm',
-      name: 'confirmUsage',
-      message: 'Would you like to include Usage instructions?',
-      default: true
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'Provide your Usage instructions:',
-      when: ({ confirmUsage }) => {
-        if (confirmUsage) {
-          return true;
-        } else {
-          return false;
-        }
-      }
-    },
-    {
-      // license information
-      type: 'checkbox',
-      name: 'licenses',
-      message: 'Which licenses did you need for this project? (Check all that apply)',
-      choices: ['MIT','Apache', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-    
-      Apache requires Apache License 2.0
-      Cloud Native Computing Foundation dictates Apache License 2.0 by default
-      GNU recommends GNU GPLv3 for most programs
-      npm packages overwhelmingly use the MIT or the very similar ISC licenses
-      OpenBSD prefers the ISC License
-      Rust crates are overwhelmingly licensed under both MIT and Apache License 2.0
-      WordPress plugins and themes must be GNU GPLv2 (or later)
-    
-    },
-    {
-      // Contributing section prompt
-      type: 'confirm',
-      name: 'confirmContribute',
-      message: 'Would you like to include a Contribute section?',
-      default: true
-    },
-    {
-      type: 'input',
-      name: 'contribute',
-      message: 'Provide your Contribute information:',
-      when: ({ confirmContribute }) => {
-        if (confirmContribute) {
-          return true;
-        } else {
-          return false;
-        }
-      }
-    },
-    // Questions section  email address
-    {
-      // add GitHub user name
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username (Required)',
-      validate: userNameInput => {
-        if (userNameInput) {
-          return true;
-        } else {
-          console.log('Please enter your user name!');
-          return false;
-        }
-      }
-    },
-    {
-      // add GitHub link
-      type: 'input',
-      name: 'link',
-      message: 'Enter the GitHub link to your project. (Required)',
-      validate: linkInput => {
-        if (linkInput) {
-          return true;
-        } else {
-          console.log('Please enter your GitHub link!');
-          return false;
-        }
-      }
-    },
-    {
-      // add email
-      type: 'input',
-      name: 'email',
-      message: 'Enter you email address. (Required)',
-      validate: emailInput => {
-        if (emailInput) {
-          return true;
-        } else {
-          console.log('Please enter your email address!');
-          return false;
-        }
-      }
-    },
-  ]);
-};
+inquirer.prompt(questions).then((resp) => {
+  console.log(resp);
+  // console.log(`Name: ${resp.yourName}`);
+});
 
-promptUser().then(answers => console.log(answers));
+  // TODO: Create a function to write README file
+  function writeToFile(fileName, data) {
+    require('./utils/generateMarkdown.js');
+  }
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+  // TODO: Create a function to initialize app
+  function init() {
 
-// TODO: Create a function to initialize app
-function init() { }
+  }
 
-// Function call to initialize app
-init();
+  // Function call to initialize app
+  init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,22 +2,47 @@
 // If there is no license, return an empty string
 
 // badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!li) {
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
-
-
-// link to my GitHub profile and user name in Questions
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  - Project Title
+  - Description
+  - Table of Contents
+  - Installation
+  ${data.installation}
+  - Usage
+  ${data.usage}
+  ## Contribute
+  ## License
+  ## Questions
+  // github username
+  // projectlink
+  // email
 
 `;
 }
